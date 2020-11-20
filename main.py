87d5c2618748f98480d7004982c99778d48ad858
Aliaksandr Samsonik <aliaksandr_samsonik@epam.com>
2020-11-20 09:59:23 +0300

    Refactoring. Add constant object. Config has been changed.

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-"""There is module which image's file with detailed information about the weather in Grodno City."""
-from configuration.config import get_config
+"""There is module which create image's file with detailed information about the weather for specific city."""
 from api.meteo_pl import create_params, get_image_by_bytes
+from config.city_config import get_city_config
+from constants.constant import get_constants
 
 if __name__ == '__main__':
-    GRODNO = "grodno"
-    with open(f"{GRODNO}.png", "wb") as file:
-        coordinate = get_config().coordinate.get(GRODNO)  # Get coordinate for specific City.
+    CITY_NAME = get_city_config().city_name
+    with open(f"{CITY_NAME}.png", "wb") as file:
+        coordinate = get_constants().coordinate.get(CITY_NAME)  # Get coordinate for specific <city_name>.
         params = create_params(coordinate)  # Create params for request.
         bytes_image = get_image_by_bytes(params)  # Send request and get image by bytes.
-        file.write(bytes_image)  # Write image to file "image.png"
+        file.write(bytes_image)  # Write image to file "<city_name>.png"
